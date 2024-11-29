308f697f354440902e7a5caadf77ff21613fc83d
utama12338 <utama12338@gmail.com>
2024-11-29 10:20:42 +0700

    d
    d

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
+app.set("views","/src/views")
+app.set("views engine","ejs")
 app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/", (req, res) => {
